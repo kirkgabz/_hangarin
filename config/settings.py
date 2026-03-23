8a6c5aede3158b5dfb970d152560a6941b048d02
kirkgabz <202380361@psu.palawan.edu.ph>
2026-03-23 23:20:35 +0800

    modifications

--- config/settings.py
+++ config/settings.py
@@ -98,6 +98,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'                   
